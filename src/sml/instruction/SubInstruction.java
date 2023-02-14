9a06d2fe116cbb52dedcf7ff84f646a7f295da86
jshamji <91161414+jshamji@users.noreply.github.com>
2023-02-14 11:41:35 +0000

    Added and amended MulInstruction, SubInstruction and a txt file.

---
+++ src/sml/instruction/SubInstruction.java
@@ -0,0 +1,38 @@
+package sml.instruction;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.RegisterName;
+
+public class SubInstruction extends Instruction {
+
+    // TODO: write a JavaDoc for the class
+
+    /**
+     * @author
+     */
+
+    private final RegisterName result;
+    private final RegisterName source;
+
+    public static final String OP_CODE = "sub";
+
+    public SubInstruction(String label, RegisterName result, RegisterName source) {
+        super(label, OP_CODE);
+        this.result = result;
+        this.source = source;
+    }
+
+    @Override
+    public int execute(Machine machine) {
+        int value1 = machine.getRegisters().get(result);
+        int value2 = machine.getRegisters().get(source);
+        machine.getRegisters().set(result, value1 - value2);
+        return NORMAL_PROGRAM_COUNTER_UPDATE;
+    }
+
+    @Override
+    public String toString() {
+        return getLabelString() + getOpcode() + " " + result + " " + source;
+    }
+}
